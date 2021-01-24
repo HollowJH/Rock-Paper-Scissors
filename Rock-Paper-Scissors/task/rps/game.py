b8f7184f1eae84deb80d4f88ecd1ec2d6f3da94b
HollowJH <KlangPX@gmail.com>
2021-01-24 17:11:07 -0500

    Making it a loop, adding the invalid input message and the option to stop

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,12 +1,19 @@
 import random
-
 options = ["rock", "paper", "scissors"]
-user = options.index(input())
-sys = random.randint(0, 2)
 
-if user == sys:
-    print(f"There is a draw ({options[sys]})")
-elif user == 0 and sys == 2 or user == 1 and sys == 0 or user == 2 and sys == 1:
-    print(f"Well done. The computer chose {options[sys]} and failed")
-else:
-    print(f"Sorry, but the computer chose {options[sys]}")
+while True:
+    user = input()
+    sys = random.randint(0, 2)
+    if user == "!exit":
+        print("Bye!")
+        break
+    else:
+        user = options.index(user) if user in options else None
+    if user == sys:
+        print(f"There is a draw ({options[sys]})")
+    elif user == 0 and sys == 2 or user == 1 and sys == 0 or user == 2 and sys == 1:
+        print(f"Well done. The computer chose {options[sys]} and failed")
+    elif sys == 0 and user == 2 or sys == 1 and user == 0 or sys == 2 and user == 1:
+        print(f"Sorry, but the computer chose {options[sys]}")
+    else:
+        print("Invalid input")
