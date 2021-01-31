ea25aef68a14741beb38c6b4ffd4281541929a5b
HollowJH <KlangPX@gmail.com>
2021-01-31 13:52:03 -0500

    Adding the option to input custom options

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,8 +1,10 @@
 import random
-options = ["rock", "paper", "scissors"]
 
 name = input("Enter your name: ").strip()
 print(f"Hello, {name}")
+choices = input().split(",")
+options = choices if choices != [""] else ["rock", "paper", "scissors"]
+
 rating = open("rating.txt")
 read = rating.readlines()
 lines = [i.strip("\n").split(" ") for i in read]
@@ -11,10 +13,15 @@ rat = [i[1] for i in lines]
 scores = dict([(names[i], rat[i]) for i in range(len(names))])
 score = int(scores[name]) if name in scores.keys() else 0
 rating.close()
+print("Okay, let's start")
 
 while True:
-    user = input(score).strip()
-    sys = random.randint(0, 2)
+    user = input().strip()
+    try:
+        user = options.index(user) if user not in ["!rating", "!exit"] else user
+    except ValueError:
+        print("Invalid input")
+        continue
     if user == "!exit":
         if name in scores.keys():
             scores[name] = score
@@ -26,20 +33,20 @@ while True:
             rating = open("rating.txt", "a")
             rating.write(f"{name} {score}\n")
             rating.close()
-        print("Bye!", scores)
+        print("Bye!")
         break
     elif user == "!rating":
         print(f"Your rating: {score}")
         continue
-    else:
-        user = options.index(user) if user in options else None
+    wins = options[user + 1:] + options[:user] if options != ["rock", "paper", "scissors"] else []
+    sys = random.randint(0, len(wins) - 1) if wins else random.randint(0, 2)
     if user == sys:
         score += 50
         print(f"There is a draw ({options[sys]})")
-    elif user == 0 and sys == 2 or user == 1 and sys == 0 or user == 2 and sys == 1:
+    elif sys >= len(wins) // 2 and wins or (user == 0 and sys == 2 or user == 1 and sys == 0 or user == 2 and sys == 1):
         score += 100
-        print(f"Well done. The computer chose {options[sys]} and failed")
-    elif sys == 0 and user == 2 or sys == 1 and user == 0 or sys == 2 and user == 1:
-        print(f"Sorry, but the computer chose {options[sys]}")
-    else:
-        print("Invalid input")
+        won = wins[sys] if wins else options[sys]
+        print(f"Well done. The computer chose {won} and failed")
+    elif sys < len(wins) // 2 and wins or (sys == 0 and user == 2 or sys == 1 and user == 0 or sys == 2 and user == 1):
+        won = wins[sys] if wins else options[sys]
+        print(f"Sorry, but the computer chose {won}")
